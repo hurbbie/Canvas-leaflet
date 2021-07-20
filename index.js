10b33597fcefa469c5cca5bcfb8266ecdd9d8de3
hurbbie <86275238+hurbbie@users.noreply.github.com>
2021-07-20 10:36:07 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var mapid = document.getElementById('mapid');
+// var mymap =L.map('mapid').setView([13.878876239563079, 100.65039509587206],10);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(mymap);
+
+// L.geoJSON(line).addTo(mymap)
